787a9fe9c40e7cdad80e47e6444df238c526ed84
Soorria Saruva <soorria.ss@gmail.com>
2021-02-09 12:58:40 +1100

    Add UserDocument type

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,8 +1,12 @@
+import { Document } from 'mongoose'
+
 interface IUser {
   _id: string
   name: string
   image?: string
   email?: string
 }
+
+export type UserDocument = IUser & Document
 
 export default IUser
